841249652038594f4f284d9f996301b5d6e6ae37
v-andrade-dev <vini.andrade0@gmail.com>
2023-11-18 18:39:13 -0300

    [learning-english] Adicionado imagens a tela de animais

--- learning_english/lib/screens/animals.dart
+++ learning_english/lib/screens/animals.dart
@@ -8,8 +8,35 @@ class Animals extends StatefulWidget {
 class _AnimalsState extends State<Animals> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Animais"),
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 1.8,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/cao.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/gato.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/leao.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/macaco.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/ovelha.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/vaca.png'),
+        )
+      ],
     );
   }
 }
