c1d5c0f268a3512401882d9d4169a30f1386768a
v-andrade-dev <vini.andrade0@gmail.com>
2023-11-19 10:46:37 -0300

    [learning-english] Implementado audio ao clicar nas imagens, tela animals

--- learning_english/lib/screens/animals.dart
+++ learning_english/lib/screens/animals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Animals extends StatefulWidget {
   @override
@@ -6,37 +7,66 @@ class Animals extends StatefulWidget {
 }
 
 class _AnimalsState extends State<Animals> {
+  final _player = AudioPlayer();
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    String _audio = '';
     return GridView.count(
       crossAxisCount: 2,
       childAspectRatio: MediaQuery.of(context).size.aspectRatio * 1.8,
       children: [
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _audio = "cao";
+            playAudio(_audio);
+          },
           child: Image.asset('assets/images/cao.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _audio = "gato";
+            playAudio(_audio);
+          },
           child: Image.asset('assets/images/gato.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _audio = "leao";
+            playAudio(_audio);
+          },
           child: Image.asset('assets/images/leao.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _audio = "macaco";
+            playAudio(_audio);
+          },
           child: Image.asset('assets/images/macaco.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _audio = "ovelha";
+            playAudio(_audio);
+          },
           child: Image.asset('assets/images/ovelha.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _audio = "vaca";
+            playAudio(_audio);
+          },
           child: Image.asset('assets/images/vaca.png'),
         )
       ],
     );
+  }
+
+  void playAudio(String audio) {
+    _player.play(AssetSource('audio/$audio.mp3'));
   }
 }
