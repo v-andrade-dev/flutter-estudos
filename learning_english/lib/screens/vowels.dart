03928188ff6a05793bf95d02be28a23532dd5c1b
v-andrade-dev <vini.andrade0@gmail.com>
2023-11-20 08:33:12 -0300

    [learning-english] Implementado tela das vogais

--- learning_english/lib/screens/vowels.dart
+++ learning_english/lib/screens/vowels.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Vowels extends StatefulWidget {
   @override
@@ -6,10 +7,54 @@ class Vowels extends StatefulWidget {
 }
 
 class _VowelsState extends State<Vowels> {
+  final _player = AudioPlayer();
+  String _audio = '';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Vogais"),
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 1.8,
+      children: [
+        GestureDetector(
+          onTap: () {
+            _audio = "a";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/a.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "e";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/e.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "i";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/i.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "o";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/o.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "u";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/u.png'),
+        )
+      ],
     );
+  }
+
+  void playerAudio(String audio) {
+    _player.play(AssetSource('audio/$audio.mp3'));
   }
 }
