c9a8a5e3ab012a3124263260131f847acc31d331
v-andrade-dev <vini.andrade0@gmail.com>
2023-11-20 08:31:43 -0300

    [learning-english] Implementado tela de numeros

--- learning_english/lib/screens/numbers.dart
+++ learning_english/lib/screens/numbers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Numbers extends StatefulWidget {
   @override
@@ -6,10 +7,61 @@ class Numbers extends StatefulWidget {
 }
 
 class _NumbersState extends State<Numbers> {
+  final _player = AudioPlayer();
+  String _audio = '';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Números"),
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 1.8,
+      children: [
+        GestureDetector(
+          onTap: () {
+            _audio = "1";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/1.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "2";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/2.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "3";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/3.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "4";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/4.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "5";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/5.png'),
+        ),
+        GestureDetector(
+          onTap: () {
+            _audio = "6";
+            playerAudio(_audio);
+          },
+          child: Image.asset('assets/images/6.png'),
+        )
+      ],
     );
+  }
+
+  void playerAudio(String audio) {
+    _player.play(AssetSource('audio/$audio.mp3'));
   }
 }
